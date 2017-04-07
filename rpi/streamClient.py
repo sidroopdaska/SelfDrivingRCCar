30c22a3afd285dfea2d2c899cdd1cae7ec70094b
Siddharth Sharma <sid3637@gmail.com>
2017-04-07 22:41:36 +0000

    updated streamClient to send jpeg format video streams

--- rpi/streamClient.py
+++ rpi/streamClient.py
@@ -1,36 +1,57 @@
+"""
+Author: sishar
+Reference:
+    PiCamera documentation
+    https://picamera.readthedocs.org/en/release-1.10/recipes2.html
+    
+"""
+
 import io
 import time
 import picamera
 import struct
 import socket
 
+# Create a TCP/IP socket and establish connection with
+# the server
 client_socket = socket.socket()
 print 'Starting connection'
-client_socket.connect(('10.166.38.64', 8000))
+
+server_address_work = ('10.166.38.64', 8000)
+server_address_home = ('192.168.0.87', 8000)
+client_socket.connect(server_address_home)
+
 print 'Connected to server'
 
+# Make a file-like object out of the client socket
 connection = client_socket.makefile('wb')
 
+# Since we are intending to process the frames after capture,
+# rather than dealing with individual JPEG captures,
+# its better to capture a video and decode the frames from the
+# resulting file.
 try:
     camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.start_preview()
-    time.sleep(2)
+    camera.resolution = (320, 240)      # pi camera resolution
+    camera.framerate = 10               # 10 frames/sec
+    time.sleep(2)                       # give 2 secs for the camera to initialise
 
     start = time.time()
     stream = io.BytesIO()
 
-    for foo in camera.capture_continuous(stream, 'jpeg'):
+    # use video-port for captures
+    for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
         connection.write(struct.pack('<L', stream.tell()))
         connection.flush()
         stream.seek(0)
         connection.write(stream.read())
-        if time.time() - start > 30:
+        if time.time() - start > 300:
             break
         stream.seek(0)
         stream.truncate()
     connection.write(struct.pack('<L', 0))
 finally:
-    print 'Closing the connection'
+    print 'Closing the connection...'
     connection.close()
     client_socket.close()
+    print 'Connection closed'
