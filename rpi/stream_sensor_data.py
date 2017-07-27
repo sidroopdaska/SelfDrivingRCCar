d02008308b2d86f0e2a07a2f8eb8d758a76b1334
Siddarth Sharma <sishar@microsoft.com>
2017-07-27 14:14:15 +0100

    capture ultrasonic sensor data on the server side

---
+++ rpi/stream_sensor_data.py
@@ -0,0 +1,6 @@
+"""stream_sensor_data.py: Transmit ultrasonic sensor data over TCP/IP socket"""
+
+import socket
+import time
+
+# TODO: Complete implementation
---
+++ test/capture_sensor_data_server_test.py
@@ -0,0 +1,34 @@
+"""capture_sensor_data_server_test.py: Capture ultrasonic sensor data on server side"""
+
+import socket
+import time
+from utils.utils import *
+
+
+class CaptureSensorDataTest(object):
+    def __init__(self):
+        self.sock = socket.socket()
+        self.sock.bind(server_address)
+        self.sock.listen(1)
+        self.connection, self.client_address = self.sock.accept()
+        self.capture_data()
+
+    def capture_data(self):
+        try:
+            print("Connection from: {0}".format(self.client_address))
+            start = time.time()
+
+            while True:
+                sensor_data = float(self.connection.recv(1024))
+                print("Distance: {0:.1f} cm".format(sensor_data))
+
+                # Test for 1 min
+                if time.time() > 60:
+                    break
+        finally:
+            self.connection.close()
+            self.sock.close()
+
+
+if __name__ == '__main__':
+    CaptureSensorDataTest()
